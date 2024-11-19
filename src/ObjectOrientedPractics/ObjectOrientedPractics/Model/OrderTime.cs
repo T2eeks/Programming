155327881a6b46b93a30baaffc47aeb636e95726
Artyom <artemrogozinzza@gmail.com>
2024-11-19 21:03:53 +0700

    Added Priority Order and OrderTime enum

---
+++ src/ObjectOrientedPractics/ObjectOrientedPractics/Model/OrderTime.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedPractics.Model
+{
+    /// <summary>
+    /// Перечисление статусов заказа
+    /// </summary>
+    public enum OrderTime
+    {
+        FromNineToElevenAM,
+        FromElevenToOnePM,
+        FromOneToThreePM,
+        FromThreeToFivePM,
+        FromFiveToSevenPM,
+        FromSevenToNinePM
+    }
+}
