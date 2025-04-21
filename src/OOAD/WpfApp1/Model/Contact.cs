e43597851390b2398d9d99ce4016f9aa1df2203b
Artyom <artemrogozinzza@gmail.com>
2025-04-22 01:28:30 +0700

    Разделено на 3 проекта

---
+++ src/OOAD/WpfApp1/Model/Contact.cs
@@ -0,0 +1,106 @@
+using System.ComponentModel;
+
+namespace Model
+{
+    /// <summary>
+    /// Класс, представляющий контакт человка.
+    /// </summary>
+    public class Contact : INotifyPropertyChanged
+    {
+
+        /// <summary>
+        /// Событие, которое вызывается при изменении свойства.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Имя человека.
+        /// </summary>
+        private string _name;
+        
+        /// <summary>
+        /// Номер телефона человека.
+        /// </summary>
+        private string _number;
+
+        /// <summary>
+        /// Электронная почта человека.
+        /// </summary>
+        private string _email;
+
+        /// <summary>
+        /// Получает или задает имя человека.
+        /// </summary>
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if(_name != value)
+                {
+                    _name = value;
+                    OnPropertyChanged(nameof(Name));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Получает или задает номер телефона человека.
+        /// </summary>
+        public string Number
+        {
+            get { return _number; }
+            set
+            {
+                if(_number != value)
+                {
+                    _number = value;
+                    OnPropertyChanged(nameof(Number));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Получает или задает электронную почту человека.
+        /// </summary>
+        public string Email
+        {
+            get { return _email; }
+            set
+            {
+                if (_email != value)
+                {
+                    _email = value;
+                    OnPropertyChanged(nameof(Email));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Вызывает событие <see cref="PropertyChanged"/> для указанного свойства.
+        /// </summary>
+        /// <param name="propertyName">Имя изменившегося свойства.</param>
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        /// <summary>
+        /// Инициализирует новый экземпляр класса <see cref="Contact"/>
+        /// </summary>
+        /// <param name="name">Имя человека.</param>
+        /// <param name="number">Номер телефона человека.</param>
+        /// <param name="email">Электронная почта человека.</param>
+        public Contact(string name, string number, string email)
+        {
+            Name = name;
+            Number = number;
+            Email = email;
+        }
+
+        /// <summary>
+        /// Инициализирует новый экземпляр класса <see cref="Contact"/>
+        /// </summary>
+        public Contact() { }
+    }
+}
---
+++ src/OOAD/WpfApp1/ViewModel/ViewModel/BaseCommand.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfApp1.ViewModel
+{
+    /// <summary>
+    /// Реализация команды с делегированием действия.
+    /// </summary>
+    internal class BaseCommand : ICommand
+    {
+        /// <summary>
+        /// Делегат выполняющий команду.
+        /// </summary>
+        private readonly Action<object> _execute;
+        /// <summary>
+        /// Делегат,определяющий можно ли выполнить команду.
+        /// </summary>
+        private readonly Func<object, bool> _canExecute;
+
+        /// <summary>
+        /// Событие, которое вызывается при изменении состояния команды.
+        /// </summary>
+        public event EventHandler CanExecuteChanged;
+
+        /// <summary>
+        /// Инициализирует новый экземпляр команды с делегированием действий.
+        /// </summary>
+        /// <param name="execute">Действие, которое выполняет команда.</param>
+        /// <param name="canExecute">Предикат, определяющий, можно ли выполнить команду.</param>
+        public BaseCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            if (execute == null)
+                throw new ArgumentNullException(nameof(execute));
+
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        /// <summary>
+        /// Определяет, может ли команда быть выполнена в текущий момент.
+        /// </summary>
+        /// <param name="parameter">Параметр, передаваемый в команду.</param>
+        /// <returns>Возвращает true, если команда может быть выполнена.</returns>
+        public bool CanExecute(object parameter)
+        {
+            if (_canExecute == null)
+                return true;
+            return _canExecute(parameter);
+        }
+
+        // <summary>
+        /// Выполняет команду.
+        /// </summary>
+        /// <param name="parameter">Параметр, передаваемый в команду.</param>
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+
+        /// <summary>
+        /// Вызывает событие CanExecuteChanged для уведомления об изменении возможности выполнения команды.
+        /// </summary>
+        public void RaiseCanExecuteChanged()
+        {
+            if (CanExecuteChanged != null)
+            {
+                CanExecuteChanged(this, EventArgs.Empty);
+            }
+        }
+    }
+}
